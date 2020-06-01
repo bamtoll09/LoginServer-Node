67fa0ae34dfbe756b4c5543f03594bd6a14b9f2a
Bamtoll09 <ryan345@nate.com>
2020-06-02 01:35:35 +0900

    v2.3a: Change Date Format && Modify Posts's Writer Shown

--- routes/posts.js
+++ routes/posts.js
@@ -14,10 +14,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/all', function(req, res) {
-  Posts.find({}, function(err, result) {
-    res.writeHead(200, {'Content-Type': 'text/json; charset=utf-8'});
-    res.write(JSON.stringify(result));
-    res.end();
+  res.writeHead(200, {'Content-Type': 'text/json; charset=utf-8'});
+
+  var result;
+  var writers = []
+  Posts.find({}, function(err, posts) {
+    result = JSON.parse(JSON.stringify(posts)); // to modify values
+    for (var i=0; i<posts.length; ++i) {
+      writers.push(posts[i].writer);
+    }
+
+    Users.find({ '_id': { $in: writers } }, function(err, users) {
+      console.log(JSON.stringify(users));
+      
+      for (var i=0; i<result.length; ++i) {
+        for (var j=0; j<users.length; ++j) {
+          console.log("WRITER: " + result[i].writer);
+          console.log("ID: " + users[j].id);
+          if (result[i].writer.toString() === users[j]._id.toString()) {
+            result[i].writer = users[j].id;
+            console.log("WRITER2: " + result[i].writer);
+            console.log("FOUND!");
+          }
+        }
+      }
+      res.write(JSON.stringify(result));
+      res.end();
+    });
   });
 });
 
@@ -32,7 +55,7 @@ router.post('/write', function(req, res) {
     _id: postId,
     title: req.body.title,
     contents: req.body.contents,
-    date: new Date(),
+    date: getTimeStamp(),
     writer: req.body.writer
   });
 // add writer by session
@@ -45,5 +68,30 @@ router.post('/write', function(req, res) {
   res.status(200).render('result', {result: "Write Success!"});
   res.end();
 });
+
+function getTimeStamp() {
+  var d = new Date();
+  var s =
+    leadingZeros(d.getFullYear(), 4) + '-' +
+    leadingZeros(d.getMonth() + 1, 2) + '-' +
+    leadingZeros(d.getDate(), 2) + ' ' +
+
+    leadingZeros(d.getHours(), 2) + ':' +
+    leadingZeros(d.getMinutes(), 2) + ':' +
+    leadingZeros(d.getSeconds(), 2);
+
+  return s;
+}
+
+function leadingZeros(n, digits) {
+  var zero = '';
+  n = n.toString();
+
+  if (n.length < digits) {
+    for (i = 0; i < digits - n.length; i++)
+      zero += '0';
+  }
+  return zero + n;
+}
 
 module.exports = router;
