e4ca79d1b11fa16e00cca1616209a795a109a1a3
Bamtoll09 <ryan345@nate.com>
2020-07-01 13:04:47 +0900

    v3.0.6a: Made checking session function where required

--- routes/posts.js
+++ routes/posts.js
@@ -20,60 +20,79 @@ router.get('/all', function(req, res, next) {
   var sess = req.session;
   // sess.destroy();
 
-  var result;
-  var writers = []
-  Posts.find({}, function(err, posts) {
-    result = JSON.parse(JSON.stringify(posts)); // to modify values
-    for (var i=0; i<posts.length; ++i) {
-      writers.push(posts[i].writer);
-    }
+  if (sess.user) {
+    var result;
+    var writers = []
+    Posts.find({}, function (err, posts) {
+      result = JSON.parse(JSON.stringify(posts)); // to modify values
+      for (var i = 0; i < posts.length; ++i) {
+        writers.push(posts[i].writer);
+      }
 
-    Users.find({ '_id': { $in: writers } }, function(err, users) {
-      // console.log(JSON.stringify(users));
-      
-      for (var i=0; i<result.length; ++i) {
-        for (var j=0; j<users.length; ++j) {
-          // console.log("WRITER: " + result[i].writer);
-          // console.log("ID: " + users[j].id);
-          if (result[i].writer.toString() === users[j]._id.toString()) {
-            result[i].writer = users[j].id;
-            // console.log("WRITER2: " + result[i].writer);
-            // console.log("FOUND!");
+      Users.find({ '_id': { $in: writers } }, function (err, users) {
+        // console.log(JSON.stringify(users));
+
+        for (var i = 0; i < result.length; ++i) {
+          for (var j = 0; j < users.length; ++j) {
+            // console.log("WRITER: " + result[i].writer);
+            // console.log("ID: " + users[j].id);
+            if (result[i].writer.toString() === users[j]._id.toString()) {
+              result[i].writer = users[j].id;
+              // console.log("WRITER2: " + result[i].writer);
+              // console.log("FOUND!");
+            }
           }
         }
-      }
-      res.write(JSON.stringify(result));
-      res.end();
+        res.write(JSON.stringify(result));
+        res.end();
+      });
     });
-  });
+  } else {
+    res.send('<h1>Please Logging in</h1>');
+    res.end();
+  }
 });
 
 router.get('/write', function(req, res) {
-  res.render('write');
+  var sess = req.session;
+
+  if (sess.user) {
+    res.render('write');
+  } else {
+    res.send('<h1>Please Logging in</h1>');
+    res.end();
+  }
 });
 
 router.post('/write', function(req, res) {
-// write post
-  const postId = new mongoose.Types.ObjectId;
-  var writer = req.body.writer;
-  if (!writer) writer = "5ece8039e45f9c2d9b4808dd";
-  Posts.create({
-    "_id": postId,
-    "title": req.body.title,
-    "contents": req.body.contents,
-    "date": getTimeStamp(),
-    "writer": writer,
-    'type': req.body.type
-  });
-// add writer by session
+  var sess = req.session;
 
-// update user posts
-  console.log("TYPEOF_POST_ID: " + postId.toString());
-  Users.findByIdAndUpdate(writer, { $push: { posts: postId.toString() } }, function() {} );
+  if (sess.user) {
+    // write post
+    const postId = new mongoose.Types.ObjectId;
+    var writer = sess.user._id;
+    if (!writer) writer = "5ece8039e45f9c2d9b4808dd";
+    Posts.create({
+      "_id": postId,
+      "title": req.body.title,
+      "contents": req.body.contents,
+      "date": getTimeStamp(),
+      "writer": writer,
+      'type': req.body.type
+    });
+    // add writer by session
 
-// success  
-  res.status(200).render('result', {result: "Write Success!"});
-  res.end();
+    // update user posts
+    console.log("TYPEOF_POST_ID: " + postId.toString());
+    Users.findByIdAndUpdate(writer, { $push: { posts: postId.toString() } }, function () { });
+
+    // success  
+    res.status(200).render('result', { result: "Write Success!" });
+    res.end();
+  } else {
+    res.send('<h1>Please Logging in</h1>');
+    res.end();
+  }
 });
 
 function getTimeStamp() {
